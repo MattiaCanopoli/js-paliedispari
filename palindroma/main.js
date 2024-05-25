712fdb6a28468d0acc5259900a64a2e202813dbe
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-25 13:02:03 +0200

    fix: funcion now compare lowerWord variable with revWord

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,31 +1,30 @@
 'use strict'
 
-//richiedo parola in input. "ripulisco" la stringa da tutti gli spazi (esterni e interni) e la converto in  minuscolo
-const inputWord = prompt('inserisci una parola')
+const inputWord = prompt('inserisci una parola') //richiedo parola in input.
 
 let outputString = 'la parola inserita non è palindroma' //definisco una variabile stringa che sarà stampata in console. il valore di default è per parole non palindrome
 
 if (inputWord.length === 0) { //verifico che l'utente abbia effettivamente dato un input
-    outputString = 'non hai inserito nulla!'
+    outputString = 'non hai inserito nulla!' //cambio il valore di outputString
 } else if (inputWord.length === 1) { //verifico che l'input abbia almeno 2 caratteri
-    outputString = 'la parola inserita è troppo corta'
-} else if (reverseWord(inputWord)) {
-    outputString = 'la parola inserita è palindorma'
+    outputString = 'la parola inserita è troppo corta' //cambio il valore di outputString
+} else if (reverseWord(inputWord)) { //verifico che la funzione invocata ritorni true
+    outputString = 'la parola inserita è palindorma' //cambio il valore di outputString
 }
 
 function reverseWord(word) { //definisco una fuzione che richiede il parametro word
 
-    word.toLowerCase() //converto tutti i caratteri in minuscolo
+    const lowerWord = word.toLowerCase() //creo una variabile che prende la parola e converte tutti i caratteri in minuscolo
 
     let revWord = '' //creo una variabile vuota per contenere la nuova stringa
 
-    for (let i = word.length - 1; i >= 0; i--) { //apro un ciclo che inizia da i = lunghezza della parola -1 e finisce quando l'indice arriva 0
-        revWord += word[i] //concateno la lettera all'indice i alla stringa revWord
+    for (let i = lowerWord.length - 1; i >= 0; i--) { //apro un ciclo che inizia da i = lunghezza della parola -1 e finisce quando l'indice arriva 0
+        revWord += lowerWord[i] //concateno la lettera all'indice i alla stringa revWord
     }
 
     let result = false //definisco una variabile result che conterrà il risultato della funzione. il default è false
 
-    if (inputWord === revWord) { //se inputWord e revWord sono uguali, il valore di result diventa true
+    if (lowerWord === revWord) { //se inputWord e revWord sono uguali, il valore di result diventa true
         result = true
     }
     return result //la funzione ritorna un valore booleano
