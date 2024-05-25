6a12f4b97fc8b0f0a3812b682cbf9d570502a7ac
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-25 12:15:14 +0200

    fix: array replaced by string in reverseWord funcion

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,35 +1,38 @@
 'use strict'
 
 //richiedo parola in input. "ripulisco" la stringa da tutti gli spazi (esterni e interni) e la converto in  minuscolo
-const inputWord = prompt('inserisci una parola').toLowerCase().replaceAll(' ','')
+const inputWord = prompt('inserisci una parola')
 
-function reverseWord(word){ //definisco una fuzione che richiede il parametro word
+function reverseWord(word) { //definisco una fuzione che richiede il parametro word
 
-    const revWordArr =[] //creo un array vuoto per contenere le singole lettere della stringa in input, passata come argomento alla funzione
+    word.toLowerCase() //converto tutti i caratteri in minuscolo
 
-    for (let i = word.length-1; i>=0; i--) { //apro un ciclo che inizia da i = lunghezza della parola -1 e finisce quando l'indice arriva 0
-        revWordArr.push(word.charAt(i)) //all'array viene aggiunto il carattere all'indice i della stringa in inputWord
+    let revWord = '' //creo un array vuoto per contenere le singole lettere della stringa in input, passata come argomento alla funzione
+
+    for (let i = word.length - 1; i >= 0; i--) { //apro un ciclo che inizia da i = lunghezza della parola -1 e finisce quando l'indice arriva 0
+        revWord += word[i]
     }
 
     let result //definisco una variabile result che conterrà il risultato della funzione
-    
-    if (inputWord===revWordArr.join('')) { //verifico che la parola in input sia uguale all'array joinato in una stringa, ovvero alla stringa in input letta da destra a sinistra
-        result = 'è palindroma' 
-    } else  {
-        result = 'non è palindroma'
+
+    if (inputWord === revWord) { //verifico che la parola in input sia uguale all'array joinato in una stringa, ovvero alla stringa in input letta da destra a sinistra
+        result = true
+    } else {
+        result = false
     }
     return result
 }
 
-let outputString
+let outputString = 'la parola inserita non è palindroma'
 
-if (inputWord.length > 1){
-    outputString= 'la parola inserita ' + reverseWord(inputWord)
-} else if (inputWord.length === 1){
-     outputString= 'hai inserito una sola lettera!'
-} else{
-    outputString= 'non hai inserito nulla!'
+if (inputWord.length === 0) {
+    outputString = 'non hai inserito nulla!'
+} else if (inputWord.length === 1) {
+    outputString = 'la parola inserita è troppo corta'
+} else if (reverseWord(inputWord)) {
+    outputString = 'la parola inserita è palindorma'
 }
+
 
 console.log(outputString)
 
